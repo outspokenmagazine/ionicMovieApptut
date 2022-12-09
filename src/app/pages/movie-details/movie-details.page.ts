492b8472c6d0fa9ece34bc0ab8fe7fc3c76577c8
henning jaeger <henning.jaeger@gmail.com>
2022-12-09 20:05:53 +0100

    add movie details page

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { movie, movieDetails, MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  movie!:movieDetails;
+  imageBaseUrl = environment.images;
+  id!: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id') as string;
+    this.movieService.getMovieDetails(this.id).subscribe((res) => {
+      this.movie = res;
+    });
   }
 
+  openHomepage(url: string) {
+    window.open(url, '_blank');
+  }
 }
